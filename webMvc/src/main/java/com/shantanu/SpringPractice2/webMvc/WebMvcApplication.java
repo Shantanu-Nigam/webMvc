fd99ffd7a411cdbc4a198011e2b4b0f6ebc5a38b
Shantanu <Shantanu@Dell>
2021-05-22 12:20:29 +0530

    Initial project structure

---
+++ webMvc/src/main/java/com/shantanu/SpringPractice2/webMvc/WebMvcApplication.java
@@ -0,0 +1,13 @@
+package com.shantanu.SpringPractice2.webMvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcApplication.class, args);
+	}
+
+}
---
+++ webMvc/src/test/java/com/shantanu/SpringPractice2/webMvc/WebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantanu.SpringPractice2.webMvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
